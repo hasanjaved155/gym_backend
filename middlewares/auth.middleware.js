34705afda5c72d6e4fa5fd134e7c1ed1cb91adca
mdmuneerhasan <md.muneerhasan@gmail.com>
2026-02-17 18:55:53 +0530

    cors changeded

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,28 +4,28 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.models.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
-  try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+  const token =
+    req.cookies?.accessToken ||
+    req.header("Authorization")?.replace("Bearer ", "");
 
-    if (!token) {
-      throw new ApiError(401, "Unauthorized request");
-    }
-    const decodecToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decodecToken?._id).select(
+  if (!token) {
+    throw new ApiError(401, "Access token missing");
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
       "-password -refreshToken",
     );
+
     if (!user) {
-      throw new ApiError(401, "Invalid AccessToken");
+      throw new ApiError(401, "Invalid access token");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(
-      500,
-      "Something went wrong during accessing accessToken",
-    );
+    throw new ApiError(401, "Invalid or expired access token");
   }
 });
