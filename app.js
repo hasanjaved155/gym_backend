9c632b55ee56e3f437c5c2a6c12117ce02e6af71
mdmuneerhasan <md.muneerhasan@gmail.com>
2026-03-02 17:23:30 +0530

    new codddd

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // for serving static files
 app.use(cookieParser());
+////////////////////////////////////////////////////////////////// new code
 
 // routes import
 import userRouter from "./routes/user.routes.js";
